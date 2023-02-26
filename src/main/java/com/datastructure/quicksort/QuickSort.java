b970f5c8a56214b8a059b85fd259e6b95330785c
dev-suren <surenrana113@gmail.com>
2023-02-26 10:18:25 +0530

    something is done here.

---
+++ src/main/java/com/datastructure/quicksort/QuickSort.java
@@ -0,0 +1,51 @@
+package com.datastructure.quicksort;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class QuickSort {
+    public static void quickSort(int arr[], int low , int high){
+        if(low>=high){
+            return;
+        }
+        int left = low;
+        int right = high;
+        int pivotElement = arr[high];
+
+        while(left<=right){
+            while(arr[left]<pivotElement){
+                left++;
+            }
+            while (arr[right]>pivotElement){
+                right--;
+            }
+            if(left<=right){
+                int tmp = arr[left];
+                arr[left]= arr[right];
+                arr[right]= tmp;
+
+                left++;
+                right--;
+            }
+        }
+        quickSort(arr,low,right);
+        quickSort(arr,left,high);
+
+    }
+
+    public static void main(String[] args) {
+        Random random = new Random();
+        int[] arr = new int[1000];
+        int i=0;
+        while(i <arr.length){
+            arr[i]= random.nextInt(1000);
+            i++;
+        }
+
+        quickSort(arr,0, arr.length-1);
+        System.out.println(Arrays.toString(arr));
+
+    }
+
+
+}
